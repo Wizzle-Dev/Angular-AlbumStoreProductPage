f8f87a6b9c74e8ae21d9c89ff59872babe07fdf6
Wizzle-Dev <55194337+Wizzle-Dev@users.noreply.github.com>
2020-10-15 13:29:54 +0100

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-despriction.component';
 import { ProductService } from './product.service';
+import { ProductTracklistingComponent } from './product-tracklisting.component';
+import { ProductListingComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+    { path: 'products', component: ProductListComponent },
+    { path: 'product/:id', component: ProductPageComponent },
+    { path: '', redirectTo: 'products', pathMatch: 'full'; }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +25,8 @@ import { ProductService } from './product.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
